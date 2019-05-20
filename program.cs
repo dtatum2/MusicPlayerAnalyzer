af5d4c20eebe64d700695b1ce0fdb2f99f121e00
dtatum2 <47043501+dtatum2@users.noreply.github.com>
2019-05-20 15:56:43 -0500

    Update program.cs

--- program.cs
+++ program.cs
@@ -5,40 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace MusicPlayerAnalyzer
-{
-    public class MusicStats
-    {
-        public String Name;
-        public String Artist;
-        public String Album;
-        public String Genre;
-        public int Size;
-        public int Time;
-        public int Year;
-        public int Plays;
-
-        public MusicStats(String name, String artist, String album, String genre, int size,
-     int time, int year, int plays)
-        {
-            Name = name;
-            Artist = artist;
-            Album = album;
-            Genre = genre;
-            Size = size;
-            Time = time;
-            Year = year;
-            Plays = plays;
-
-        }
-    }
-}
-        override public string ToString()
-        {
-            return String.Format("Name: {0}, Artist: {1}, Album: {2}, Genre: {3}, Size: {4}, Time: {5}, Year: {6}, Plays: {7}", Name, Artist, Album, Genre, Size, Time, Year, Plays);
-        }
-
-    }
     class Program
     {
         static void Main(string[] args)
@@ -160,8 +126,40 @@ namespace MusicPlayerAnalyzer
         }
     }
 }
+namespace MusicPlayerAnalyzer
+{
+    public class MusicStats
+    {
+        public String Name;
+        public String Artist;
+        public String Album;
+        public String Genre;
+        public int Size;
+        public int Time;
+        public int Year;
+        public int Plays;
 
+        public MusicStats(String name, String artist, String album, String genre, int size,
+     int time, int year, int plays)
+        {
+            Name = name;
+            Artist = artist;
+            Album = album;
+            Genre = genre;
+            Size = size;
+            Time = time;
+            Year = year;
+            Plays = plays;
 
+        }
+    }
+}
+        override public string ToString()
+        {
+            return String.Format("Name: {0}, Artist: {1}, Album: {2}, Genre: {3}, Size: {4}, Time: {5}, Year: {6}, Plays: {7}", Name, Artist, Album, Genre, Size, Time, Year, Plays);
+        }
+
+    }
 
 
 
